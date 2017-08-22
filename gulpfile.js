0006eb4e957c4da04114f4755e9831f1212b83e2
DenisCaeyers <denis.caeyers@gmail.com>
2017-08-22 14:45:27 +0200

    Commit Gulp SASS

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+// Global Packages
+var gulp = require('gulp');
+
+// Stylesheet Packages
+var sass = require('gulp-sass');
+
+// Stylesheet Tasks
+gulp.task('sass-dev', function () {
+    return gulp.src([
+        './static/scss/*.scss',
+      ])
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./static/css'));
+});
+
+// Watch Tasks
+gulp.task('watch', function () {
+    gulp.watch('./static/scss/*.scss', ['sass-dev']);    
+});
+
+//Default task
+gulp.task('default', ['sass-dev']);
+
