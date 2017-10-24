d2fde1c42b531f6476331f38808a5de109ec5727
DenisCaeyers <denis.caeyers@gmail.com>
2017-10-24 16:07:17 +0200

    Upgraded Gulpfile to include IconFont

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ var server = {
     https: false
 }
 
+var fontName = 'qp-icons';
+var fontCssClass = "qp-icon";
+
 // Global Packages
 var gulp = require('gulp');
 var rename = require('gulp-rename');
@@ -13,6 +16,11 @@ var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
+
+// Iconfont Packages
+var imagemin = require('gulp-imagemin');
+var iconfont = require('gulp-iconfont');
+var iconfontcss = require('gulp-iconfont-css');
 
 // Webserver packages
 var browserSync = require('browser-sync').create();
@@ -36,15 +44,40 @@ gulp.task('post-css',['sass-dev'], function () {
       .pipe(browserSync.stream());      
 });
 
+// Iconfont tasks
+gulp.task('iconfont',['imagemin'], function() {
+    gulp.src(['./static/assets/svg/font/*.svg'])
+        .pipe(iconfontcss({
+            fontName: fontName,
+            targetPath: '../../scss/PUB/_Icons.scss',
+            fontPath: '../../static/assets/fonts/',
+            cssClass: fontCssClass
+        }))
+        .pipe(iconfont({
+            fontName: fontName,
+            // Remove woff2 if you get an ext error on compile
+            formats: ['svg', 'ttf', 'eot', 'woff', 'woff2'],
+            normalize: true,
+            fontHeight: 1001
+        }))
+    .pipe(gulp.dest('./static/assets/fonts/'))
+});
+
+gulp.task('imagemin', function(){
+    gulp.src('./static/assets/svg/font/*.svg')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./static/assets/svg/font/'))
+});
 
 
 // Watch Tasks
-gulp.task('watch',['post-css'], function () {
+gulp.task('watch',['iconfont','post-css'], function () {
     browserSync.init({
         server: "."
     });
     gulp.watch('./static/scss/**/*.scss', ['post-css']);
     gulp.watch("./**/*.html").on('change', browserSync.reload);
+    gulp.watch('./static/assets/svg/font/*.svg', ['iconfont']);
 });
 
 //Default task
