e0b20198ff4a6b4a43bafdf29a2938053cb2312b
kaceg <31242444+kaceg@users.noreply.github.com>
2017-12-07 13:18:27 +0000

    E-Shop: Value Cards - Step 1

--- static/js/custom.js
+++ static/js/custom.js
@@ -416,7 +416,7 @@ $(document).ready(function($) {
 			$(value).css("background-image", "url('" + url + "')");
 		}
 	});
-	
+
 	/*************************
 		QUANTITY SELECTOR
 	*************************/
